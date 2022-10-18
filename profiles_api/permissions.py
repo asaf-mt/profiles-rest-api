aac5a9af6fac9f32b2e17c0a43ac819996e351de
asaf <asaf@LAPTOP-KO5OJNCD.localdomain>
2022-10-18 15:50:00 +0300

    added user profile feed api

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -1,3 +1,4 @@
+from urllib import request
 from rest_framework import permissions
 
 
@@ -10,3 +11,14 @@ class UpdateOwnProfile(permissions.BasePermission):
       return True
 
     return obj.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+  """Allow user to update their owe status"""
+
+  def has_object_permission(self, request, view, obj):
+    """Check user is trying to update their owe status"""
+
+    if request.method in permissions.SAFE_METHODS:
+      return True
+
+    return obj.user_profile.id == request.user.id
